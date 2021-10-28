3c45e1c86865935843ae40924d0192bc3e8bcb59
Shahidul <mdshahidulridoy@gmail.com>
2021-10-29 02:11:59 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import './Header.css'
+import { Link } from 'react-router-dom';
+import hamburgerIcon from '../../media/hamburger-icon.svg';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Hello! I am from header</h1>
+        <div className="header">
+            <Link to="/" className="header-logo">
+                <img src={hamburgerIcon} alt="Foody hamburger icon" />
+                <h1>Foody</h1>
+            </Link>
+
+            <div className="header-navlist">
+                <Link to="/home">Home</Link>
+                <Link to="/foods">Browse Foods</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+            <div className="header-right">
+                <Link to="/login" className="btn-primary">
+                    <i class="fi fi-rr-sign-in-alt"></i> <span>Login</span>
+                </Link>
+            </div>
         </div>
     );
 };
